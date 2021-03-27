a6dd2758c80241db96b71b342b906ed4ad059c48
Jurij Ambros <jurij.ambros@gmail.com>
2021-03-27 09:15:45 +0200

    letnelių mygtukas

--- 3PL1_DatabaseCreate/Form1.Designer.cs
+++ 3PL1_DatabaseCreate/Form1.Designer.cs
@@ -28,13 +28,33 @@ namespace _3PL1_DatabaseCreate
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btnLenteliųKūrimas = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnLenteliųKūrimas
+			// 
+			this.btnLenteliųKūrimas.Location = new System.Drawing.Point(12, 12);
+			this.btnLenteliųKūrimas.Name = "btnLenteliųKūrimas";
+			this.btnLenteliųKūrimas.Size = new System.Drawing.Size(144, 23);
+			this.btnLenteliųKūrimas.TabIndex = 0;
+			this.btnLenteliųKūrimas.Text = "Lentelių kūrimas";
+			this.btnLenteliųKūrimas.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(395, 339);
+			this.Controls.Add(this.btnLenteliųKūrimas);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnLenteliųKūrimas;
 	}
 }
 
